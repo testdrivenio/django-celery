10361818a0f1a5996ded546b4232fdaa07f88119
Michael Herman <hermanmu@gmail.com>
2020-03-28 19:32:49 -0400

    add celery

--- project/core/settings.py
+++ project/core/settings.py
@@ -115,3 +115,6 @@ USE_TZ = True
 STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
