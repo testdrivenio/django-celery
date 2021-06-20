cba9fb030ca048d044245c296c833d5bdc80b2af
Michael Herman <hermanmu@gmail.com>
2021-06-19 18:16:16 -0600

    update dependencies

---
+++ project/tasks/sample_tasks.py
@@ -0,0 +1,11 @@
+# project/tasks/sample_tasks.py
+
+import time
+
+from celery import shared_task
+
+
+@shared_task
+def create_task(task_type):
+    time.sleep(int(task_type) * 10)
+    return True
--- project/tasks/views.py
+++ project/tasks/views.py
@@ -1,6 +1,9 @@
+from celery.result import AsyncResult
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+
+from tasks.sample_tasks import create_task
 
 
 def home(request):
@@ -11,9 +14,16 @@ def home(request):
 def run_task(request):
     if request.POST:
         task_type = request.POST.get("type")
-        return JsonResponse({"task_type": task_type}, status=202)
+        task = create_task.delay(int(task_type))
+        return JsonResponse({"task_id": task.id}, status=202)
 
 
 @csrf_exempt
 def get_status(request, task_id):
-    return JsonResponse({"task_id": task_id}, status=200)
+    task_result = AsyncResult(task_id)
+    result = {
+        "task_id": task_id,
+        "task_status": task_result.status,
+        "task_result": task_result.result
+    }
+    return JsonResponse(result, status=200)
