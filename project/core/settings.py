3f3b18f745d1fd99782b5aef9dcc931ec2fdb28b
Michael Herman <hermanmu@gmail.com>
2022-12-21 11:10:03 -0600

    updates

--- project/core/settings.py
+++ project/core/settings.py
@@ -125,3 +125,6 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_RESULT_BACKEND = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
--- project/tests/test_tasks.py
+++ project/tests/test_tasks.py
@@ -1,7 +1,48 @@
+import json
+from unittest.mock import patch
+
 from django.urls import reverse
+
+from tasks import sample_tasks
 
 
 def test_home(client):
     url = reverse("home")
     response = client.get(url)
     assert response.status_code == 200
+
+
+def test_task():
+    assert sample_tasks.create_task.run(1)
+    assert sample_tasks.create_task.run(2)
+    assert sample_tasks.create_task.run(3)
+
+
+@patch("tasks.sample_tasks.create_task.run")
+def test_mock_task(mock_run):
+    assert sample_tasks.create_task.run(1)
+    sample_tasks.create_task.run.assert_called_once_with(1)
+
+    assert sample_tasks.create_task.run(2)
+    assert sample_tasks.create_task.run.call_count == 2
+
+    assert sample_tasks.create_task.run(3)
+    assert sample_tasks.create_task.run.call_count == 3
+
+
+def test_task_status(client):
+    response = client.post(reverse("run_task"), {"type": 0})
+    content = json.loads(response.content)
+    task_id = content["task_id"]
+    assert response.status_code == 202
+    assert task_id
+
+    response = client.get(reverse("get_status", args=[task_id]))
+    content = json.loads(response.content)
+    assert content == {"task_id": task_id, "task_status": "PENDING", "task_result": None}
+    assert response.status_code == 200
+
+    while content["task_status"] == "PENDING":
+        response = client.get(reverse("get_status", args=[task_id]))
+        content = json.loads(response.content)
+    assert content == {"task_id": task_id, "task_status": "SUCCESS", "task_result": True}
